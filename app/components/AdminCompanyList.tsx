d28b300676a390d109e003efc0966f132869124d
mido4499 <78541737+mido4499@users.noreply.github.com>
2026-06-27 11:17:27 -0700

    added an option in the admin page to remove a company from listing

--- app/components/AdminCompanyList.tsx
+++ app/components/AdminCompanyList.tsx
@@ -10,6 +10,7 @@ type Props= {
 }
 export default function AdminCompanyList({companies}: Props) {
     const [loading, setLoading] = useState<string|null>(null);
+    const [searchCompany, setSearchCompany] = useState("");
     const router = useRouter();
 
     async function handleDecision(companyid: string, decision: 'approved' | 'rejected') {
@@ -27,8 +28,37 @@ export default function AdminCompanyList({companies}: Props) {
         router.refresh();
     }
 
+    async function removeCompany(slug: string) {
+        const res = await fetch(
+            `/api/companies/${slug}`,
+            {
+                method: 'GET',
+                headers: {'Content-Type': 'application/json'},
+            }
+        )
+        const company = await res.json();
+        await fetch(
+            `/api/admin/companies/${company.id}`,
+            {
+                method: 'PATCH',
+                headers: {'Content-Type': 'application/json'},
+                body: JSON.stringify({status: 'rejected'}),
+            }
+        )
+    }
+
     return (
         <div className="flex flex-col gap-4">
+            <div className="w-full flex">
+                <input
+                    type="text"
+                    placeholder="search for a company"
+                    className="rounded bg-white w-1/2"
+                    value={searchCompany}
+                    onChange={e=>setSearchCompany(e.target.value)}
+                />
+                <button className="w-1/2" onClick={()=>removeCompany(searchCompany)}>Remove</button>
+            </div>
             <h1 className="text-3xl text-(--slate)">Pending Requests</h1>
             <div className="flex flex-col gap-2">
                 {companies.map((company)=>(
--- auth.ts
+++ auth.ts
@@ -1,10 +1,9 @@
 import NextAuth from "next-auth";
 import Google from "next-auth/providers/google";
 import Credentials from "next-auth/providers/credentials";
-import { PrismaClient } from "@prisma/client";
+import { prisma } from "@/src/lib/db";
 import bcrypt from 'bcryptjs';
 
-const prisma = new PrismaClient();
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
     providers: [
@@ -15,17 +14,23 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
                 password: {label: 'Password', type: 'password'}
             },
             async authorize(credentials) {
-                if (!credentials?.email || !credentials?.password) return null; // Checks both email and password are provided
+                if (!credentials?.email || !credentials?.password) {
+                    console.log("Missing Credentials---------------_!!!!!!!!!!!!!!!!!!!!!!!!!");
+                    return null;
+                }; // Checks both email and password are provided
 
                 const user = await prisma.user.findUnique({
                     where: {email: credentials.email as string} // Gets the user with this email from the database
                 });
 
-                if (!user || !user.password) return null; // If the user does not exist or a password doesn't exist, null
+                if (!user) {console.log("user not found"); return null;}
+                if (!user.password){console.log("user doesn't have a password---------------_!!!!!!!!!!!!!!!!!!!!!!!!!")}
+
+                if (!user || !user.password) {return null}; // If the user does not exist or a password doesn't exist, null
 
                 const isValid = await bcrypt.compare(credentials.password as string, user.password); // Validates the password
 
-                if (!isValid) return null; 
+                if (!isValid) {console.log("passwords didn't match---------------_!!!!!!!!!!!!!!!!!!!!!!!!!", user.email, "--------", user.password); return null}; 
 
                 return user;
             }
